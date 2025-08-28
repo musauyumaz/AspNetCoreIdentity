367def7a1f0f753750485299b2b5fe6dc5fe120c
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-08-28 10:41:34 +0300

    feat: add IdentityDbContext for managing ASP.NET Core Identity entities

---
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp/server/src/Infrastructure/AspNetCoreIdentityApp.Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCoreIdentityApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityApp.Persistence.Contexts;
+
+public sealed class AppDbContext(DbContextOptions options) : IdentityDbContext<User, Role, string>(options)
+{
+}
---
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp/server/src/Infrastructure/AspNetCoreIdentityApp.Persistence/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreIdentityApp.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreIdentityApp.Persistence;
+
+public static class ServiceRegistration
+{
+    public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("SQLConnection")));
+    }
+}
