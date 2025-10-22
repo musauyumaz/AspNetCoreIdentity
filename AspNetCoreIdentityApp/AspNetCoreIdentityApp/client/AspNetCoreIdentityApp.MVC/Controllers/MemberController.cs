6fa80eea1360529dc1d2c08de8501bedc51bc1c5
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-10-22 22:04:14 +0300

    feat: add MemberController Index action and create SignIn and Index views for user login and welcome display

--- AspNetCoreIdentityApp/AspNetCoreIdentityApp/client/AspNetCoreIdentityApp.MVC/Controllers/MemberController.cs
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp/client/AspNetCoreIdentityApp.MVC/Controllers/MemberController.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 namespace AspNetCoreIdentityApp.MVC.Controllers
 {
+    [Authorize]
     public class MemberController : Controller
     {
         public async Task SignOut()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
         }
     }
 }
