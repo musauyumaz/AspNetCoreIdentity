b666a8346fe1e0dc76346f0aebec9dc2b53596d3
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-10-26 14:42:07 +0300

    Refactor: Rename User.Birthday to User.BirthDate and DTOs

---
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp/client/AspNetCoreIdentityApp.MVC/Models/DTOs/EditUserRequestDTO.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.MVC.Models.DTOs
+{
+    public record EditUserRequestDTO
+    {
+        [Required(ErrorMessage ="Kullanıcı Ad alanı boş bırakılamaz")]
+        [Display(Name = "Kullanıcı Adı :")]
+        public string UserName { get; init; }
+
+        [Required(ErrorMessage ="Email alanı boş bırakılamaz")]
+        [EmailAddress(ErrorMessage ="Geçersiz Email Adresi")]
+        [Display(Name = "Email :")]
+        public string Email { get; init; }
+
+        [Required(ErrorMessage ="Telefon alanı boş bırakılamaz")]
+        [Display(Name = "Telefon :")]
+        public string PhoneNumber { get; init; }
+
+        [Display(Name = "Doğum Tarihi :")]
+        public DateTime? BirthDate { get; init; }
+
+        [Display(Name = "Şehir :")]
+        public string? City { get; init; }
+
+        [Display(Name = "Profil Resmi :")]
+        public IFormFile? Picture { get; init; }
+
+        [Display(Name = "Cinsiyet :")]
+        public byte? Gender { get; init; }
+
+    }
+}
