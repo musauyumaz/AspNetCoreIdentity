ffa549c7c4dd98d8393d468a691282af8c6a9673
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-10-29 17:25:52 +0300

    feat: Implement role creation endpoint with mediator pattern

---
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp/server/src/Core/AspNetCoreIdentityApp.Application/Features/Roles/Commands/Add/RoleAddCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using AspNetCoreIdentityApp.Application.Commons.Results;
+using AspNetCoreIdentityApp.Domain.Entities;
+using Mapster;
+using Mediator;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreIdentityApp.Application.Features.Roles.Commands.Add
+{
+    public record RoleAddCommandRequest(string Name) : IRequest<Result<string>>;
+    public sealed class RoleAddCommandHandler(RoleManager<Role> _roleManager) : IRequestHandler<RoleAddCommandRequest, Result<string>>
+    {
+        public async ValueTask<Result<string>> Handle(RoleAddCommandRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _roleManager.CreateAsync(request.Adapt<Role>());
+
+            return result.Succeeded
+                ? Result<string>.Success("Rol başarıyla eklendi.")
+                : Result<string>.Fail(string.Join(", ", result.Errors.Select(e => e.Description)));
+        }
+    }
+}
---
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp/server/src/Presentation/AspNetCoreIdentityApp.API/Controllers/RolesController.cs
@@ -0,0 +1,19 @@
+﻿using AspNetCoreIdentityApp.Application.Commons.Results;
+using AspNetCoreIdentityApp.Application.Features.Roles.Commands.Add;
+using Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController(IMediator _mediator) : ControllerBase
+    {
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add(RoleAddCommandRequest roleAddCommandRequest)
+        {
+            Result<string> data = await _mediator.Send(roleAddCommandRequest);
+            return StatusCode((int)data.StatusCode, data);
+        }
+    }
+}
