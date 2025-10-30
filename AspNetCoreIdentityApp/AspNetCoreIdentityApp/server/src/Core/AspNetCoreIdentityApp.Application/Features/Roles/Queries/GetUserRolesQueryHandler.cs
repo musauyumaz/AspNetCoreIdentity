7c18aa336dbe394cd80e5d6d009ca0a5ecf684b4
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-10-30 22:21:54 +0300

    feat: Implement GetUserRoles query and DTO

---
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp/server/src/Core/AspNetCoreIdentityApp.Application/Features/Roles/Queries/GetUserRolesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreIdentityApp.Application.Commons.Results;
+using AspNetCoreIdentityApp.Application.Features.Roles.DTOs;
+using AspNetCoreIdentityApp.Domain.Entities;
+using Mediator;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreIdentityApp.Application.Features.Roles.Queries
+{
+    public record GetUserRolesQueryRequest(string UserId) : IRequest<Result<Dictionary<string, GetUserRoleDTO>>>;
+    public sealed class GetUserRolesQueryHandler(RoleManager<Role> _roleManager, UserManager<User> _userManager) : IRequestHandler<GetUserRolesQueryRequest, Result<Dictionary<string, GetUserRoleDTO>>>
+    {
+        public async ValueTask<Result<Dictionary<string, GetUserRoleDTO>>> Handle(GetUserRolesQueryRequest request, CancellationToken cancellationToken)
+        {
+            User? user = await _userManager.FindByIdAsync(request.UserId);
+            if (user == null) 
+                return Result<Dictionary<string, GetUserRoleDTO>>.Fail("Kullanıcı bulunamadı.", System.Net.HttpStatusCode.NotFound);
+            List<Role>? allRoles = _roleManager.Roles.ToList();
+            HashSet<string>? userRoleIds = (await _userManager.GetRolesAsync(user)).ToHashSet();
+
+            Dictionary<string, GetUserRoleDTO>? roles = allRoles.ToDictionary(
+                r => r.Id,
+                r => new GetUserRoleDTO(r.Name, userRoleIds.Contains(r.Name)));
+            
+            return Result<Dictionary<string, GetUserRoleDTO>>.Success(new Dictionary<string, GetUserRoleDTO>(roles), System.Net.HttpStatusCode.OK);
+        }
+    }
+}
