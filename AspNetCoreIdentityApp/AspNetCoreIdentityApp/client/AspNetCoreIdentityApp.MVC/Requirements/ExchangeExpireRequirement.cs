176c400bce956b4b4c08b31bc10f99ae151081ad
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-11-06 22:13:12 +0300

    feat: Implement ExchangeExpireRequirement for authorization

---
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp/client/AspNetCoreIdentityApp.MVC/Requirements/ExchangeExpireRequirement.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreIdentityApp.MVC.Requirements
+{
+    public class ExchangeExpireRequirement : IAuthorizationRequirement
+    {
+    }
+
+    public class ExchangeExpireRequirementHandler : AuthorizationHandler<ExchangeExpireRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ExchangeExpireRequirement requirement)
+        {
+            var hasExchangeExpireClaim = context.User.HasClaim(c => c.Type == "ExchangeExpireDate");
+
+            if (!hasExchangeExpireClaim)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            var exchangeExpireDate = context.User.FindFirst("ExchangeExpireDate")?.Value;
+
+            if (DateTime.TryParse(exchangeExpireDate, out DateTime result) && DateTime.Now > result)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
